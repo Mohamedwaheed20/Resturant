572a6e5abb480ffcf133ce9d6803e914cd58482c
Mohamedwaheed20 <mohamedwaheed202005@gmail.com>
2025-11-30 07:33:31 -0800

    fix vercel2  deployment

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 
-export default function connectToDB() {
-    try {
-        mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Failed to connect to MongoDB:", error);
-    }
+export default async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 10000,
+    });
+
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error.message);
+    process.exit(1); // عشان السيرفر يقف لو فيه مشكلة
+  }
 }
--- src/moudela/dashboard/dashboard-controller.js
+++ src/moudela/dashboard/dashboard-controller.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import * as dashboardService from "./service/dashboard-service.js"; 
 import { errorHandler } from "../../middelware/error-handler-middelware.js";
-import { authantication_middelware } from "../../middelware/authantication-middelware.js";
+// import { authantication_middelware } from "../../middelware/authantication-middelware.js";
 
 const dashboardController = Router();
 
